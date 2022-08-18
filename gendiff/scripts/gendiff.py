7c463e582f4b9127bcd6d2290406034983d545c2
Dima343316 <mitya.volkov.2021@mail.ru>
2022-08-18 14:26:26 +0300

    второй этап

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 import argparse
+
+
 def main():
     program_name = "Compares two configuration files and shows a difference."
     parser = argparse.ArgumentParser(description=program_name)
     first_file = parser.add_argument("first_file")
     second_file = parser.add_argument("second_file")
+    format_file = parser.add_argument("-f", "--format",
+            help="set format of output")
     args = parser.parse_args()
+
+
 if __name__ == '__main__':
     main()
 
